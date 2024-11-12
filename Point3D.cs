c238be201558374b1dbbab0dbffed78a5237022c
tran.vo <tran.vo@PCDG7PM>
2024-11-12 16:28:07 +0700

    Add project files.

---
+++ Point3D.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PointGeometrics
+{
+    public class Point3D
+    {
+        public decimal X { get; set; } = 0;
+        public decimal Y { get; set; } = 0;
+        public decimal Z { get; set; } = 0;
+
+        public Point3D(decimal pX, decimal pY, decimal pZ)
+        {
+            this.X = pX;
+            this.Y = pY;
+            this.Z = pZ;
+        }
+
+        public Point3D Add(Point3D that)
+        {
+            return new Point3D(pX: this.X + that.X, pY: this.Y + that.Y, pZ: this.Z + that.Z); 
+        }
+
+        public Point3D Substract(Point3D that)
+        {
+            return new Point3D(pX: this.X - that.X, pY: this.Y - that.Y, pZ: this.Z - that.Z);
+        }
+
+        public Point3D Multiply(decimal k)
+        {
+            return new Point3D(pX: this.X * k, pY: this.Y * k, pZ: this.Z * k);
+        }
+
+        public Point3D Divide(decimal k)
+        {
+            return new Point3D(pX: this.X / k, pY: this.Y / k, pZ: this.Z / k);
+        }
+
+        public decimal ComputeDistance(Point3D that)
+        {
+            decimal XComponent = (this.X - that.X) * (this.X - that.X);
+            decimal YComponent = (this.Y - that.Y) * (this.Y - that.Y);
+            decimal ZComponent = (this.Z - that.Z) * (this.Z - that.Z);
+
+            decimal Distance = (decimal) Math.Round(Math.Sqrt((double) (XComponent + YComponent + ZComponent)), 4);
+            return Distance;
+        }
+
+        public override string ToString()
+        {
+            return "Point3D( " + X + ", " + Y + ", " + Z + ")";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using PointGeometrics;
+
+Point3D P1 = new Point3D(1, 2, 3);
+Point3D P2 = new Point3D(4, 5, 6);
+
+Console.WriteLine("Result of: " + P1 + " + " + P2 + ":");
+Console.WriteLine(P1.Add(P2));
+
+Console.WriteLine("Result of: " + P1 + " - " + P2 + ":");
+Console.WriteLine(P1.Substract(P2));
+
+Console.WriteLine("Result of: " + P1 + " * " + 5 + ":");
+Console.WriteLine(P1.Multiply(5));
+
+Console.WriteLine("Result of: " + P1 + " / " + 10 + ":");
+Console.WriteLine(P1.Divide(10));
+
+Console.WriteLine("Distance from: " + P1 + " to: " + P2 + ":");
+Console.WriteLine(P1.ComputeDistance(P2));
