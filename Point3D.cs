2e3bff3a787acf6cf17d64587332c89e81b626b2
tran.vo <tran.vo@PCDG7PM>
2024-11-13 09:07:09 +0700

    Add data validation

--- Point3D.cs
+++ Point3D.cs
@@ -9,10 +9,12 @@ namespace PointGeometrics
 {
     public class Point3D
     {
+        // 3D coordinations of points
         public decimal X { get; set; } = 0;
         public decimal Y { get; set; } = 0;
         public decimal Z { get; set; } = 0;
 
+        // Constructor
         public Point3D(decimal pX, decimal pY, decimal pZ)
         {
             this.X = pX;
@@ -20,28 +22,55 @@ namespace PointGeometrics
             this.Z = pZ;
         }
 
+        // Addition
+
         public Point3D Add(Point3D that)
         {
+            if (that is null)
+            {
+                throw new NullReferenceException("The argument 3D point can not be null.");
+            }
             return new Point3D(pX: this.X + that.X, pY: this.Y + that.Y, pZ: this.Z + that.Z); 
         }
 
+        // Substraction
+
         public Point3D Substract(Point3D that)
         {
+            if (that is null)
+            {
+                throw new NullReferenceException("The argument 3D point can not be null.");
+            }
             return new Point3D(pX: this.X - that.X, pY: this.Y - that.Y, pZ: this.Z - that.Z);
         }
+
+        // Multiplication
 
         public Point3D Multiply(decimal k)
         {
             return new Point3D(pX: this.X * k, pY: this.Y * k, pZ: this.Z * k);
         }
 
+        // Division
+
         public Point3D Divide(decimal k)
         {
+            if (k == 0)
+            {
+                throw new DivideByZeroException("The argument decimal can not equal 0.");
+            }
             return new Point3D(pX: this.X / k, pY: this.Y / k, pZ: this.Z / k);
         }
 
+        // Distance calculation
+
         public decimal ComputeDistance(Point3D that)
         {
+            if (that is null)
+            {
+                throw new NullReferenceException("The argument 3D point can not be null.");
+            }
+
             decimal XComponent = (this.X - that.X) * (this.X - that.X);
             decimal YComponent = (this.Y - that.Y) * (this.Y - that.Y);
             decimal ZComponent = (this.Z - that.Z) * (this.Z - that.Z);
@@ -50,6 +79,7 @@ namespace PointGeometrics
             return Distance;
         }
 
+        // Console output
         public override string ToString()
         {
             return "Point3D( " + X + ", " + Y + ", " + Z + ")";
--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿using PointGeometrics;
 
+// TestCase for point P1(1,2,3) and point P2(4,5,6)
 Point3D P1 = new Point3D(1, 2, 3);
 Point3D P2 = new Point3D(4, 5, 6);
 
+// Addition
 Console.WriteLine("Result of: " + P1 + " + " + P2 + ":");
 Console.WriteLine(P1.Add(P2));
 
+// Substraction
 Console.WriteLine("Result of: " + P1 + " - " + P2 + ":");
 Console.WriteLine(P1.Substract(P2));
 
+// Multiplication
 Console.WriteLine("Result of: " + P1 + " * " + 5 + ":");
 Console.WriteLine(P1.Multiply(5));
 
+// Division
 Console.WriteLine("Result of: " + P1 + " / " + 10 + ":");
 Console.WriteLine(P1.Divide(10));
 
+// Computing distance between 2 points
 Console.WriteLine("Distance from: " + P1 + " to: " + P2 + ":");
 Console.WriteLine(P1.ComputeDistance(P2));
